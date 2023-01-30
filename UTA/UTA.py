baf9aa6ce90a2ac86a124a2075dafc044e4f9e61
GORCEIX Antoine <gorceixantoine@macbook-air-de-gorceix-2.home>
2023-01-30 16:13:16 +0100

    Version quasi finale UTA

--- UTA/UTA.py
+++ UTA/UTA.py
@@ -12,6 +12,10 @@ from UTA_functions import add_constraints
 from UTA_functions import add_objective_function
 from UTA_functions import s_score
 from UTA_functions import si_k
+from UTA_functions import create_variables_app
+from UTA_functions import add_constraints_app
+from UTA_functions import s_score_app
+from UTA_functions import get_cl
 
 # Data directory
 DATA_PATH = Path("./Preorder_instance.csv")
@@ -22,6 +26,11 @@ I1=I['Classe'].to_numpy()
 I0=I.drop(['Classe'], axis=1).to_numpy()
 I1=[[k,I1[k]] for k in range(len(I1))]
 EPSILON = 0.001
+
+############################################################################################
+######################## MODELE AVEC COEFFICIENTS NON APPRIS ###############################
+############################################################################################
+
 def create_model(list_alternatives,partial_categories,EPSILON,L):
     m=Model("Preferences")
     
@@ -34,6 +43,7 @@ def create_model(list_alternatives,partial_categories,EPSILON,L):
     m=add_objective_function(m=m,variable=variables)
     
     return m
+
 
 
 m=create_model(I0,I1,EPSILON,2)
@@ -58,16 +68,78 @@ def identify_classes(instances,model,X): # On crée une fonction qui va attribue
     g=np.zeros(len(V1))
     Y=np.c_[V1,g]
     Y=pd.DataFrame(Y,columns =['profit','projects_done','long_proj_duration','Classe'])
-    
+    c1=0
+    c2=0
+    c3=0
     for k in range(V1.shape[0]):
         if s_score(V1[k],l,X,2)==0 :
             Y['Classe'][k]='Solution non acceptable'
+            c1+=1
         elif s_score(V1[k],l,X,2)==1 :
             Y['Classe'][k]='Solution neutre'
+            c2+=1
         else :
             Y['Classe'][k]='Solution satisfaisante'
+            c3+=1
+    #print(c1/V1.shape[0],c2/V1.shape[0],c3/V1.shape[0])
 
     Y.to_csv('Classification_instances.csv', index=False)
     return
 
 identify_classes(instances=V,model=m,X=X)
+
+############################################################################################
+######################## MODELE AVEC COEFFICIENTS APPRIS ###################################
+############################################################################################
+
+
+
+def create_model_app(list_alternatives,partial_categories,EPSILON,L):
+    m=Model("Preferences")
+    
+    instances= Built_model_instances(list_alternatives=list_alternatives,L=L)
+    X, X1 =instances
+    m , variables = create_variables_app(m=m,list_alternatives=list_alternatives,L=L)
+    
+    m=add_constraints_app(list_alternatives=list_alternatives,m=m,variables=variables,X=X,X1=X1,partial_categories=partial_categories,EPSILON=EPSILON,L=L)
+    
+    m=add_objective_function(m=m,variable=variables)
+    
+    return m
+
+m_app=create_model_app(I0,I1,EPSILON,2)
+m_app.optimize()
+
+l=si_k(m_app,X,u=3)
+cl=get_cl(m_app,X)
+
+def identify_classes_appris(instances,model,X,cl): # On crée une fonction qui va attribuer des classes à nos instance.
+    
+    V1=instances.to_numpy()
+    V1=V1[1:]
+    V1=(V1.T[1:]).T
+
+    l=si_k(model,X,u=3)
+    g=np.zeros(len(V1))
+    Y=np.c_[V1,g]
+    Y=pd.DataFrame(Y,columns =['profit','projects_done','long_proj_duration','Classe'])
+    print(cl)
+    c1=0
+    c2=0
+    c3=0
+    for k in range(V1.shape[0]):
+        if s_score_app(V1[k],l,X,2,cl)==0 :
+            Y['Classe'][k]='Solution non acceptable'
+            c1+=1
+        elif s_score_app(V1[k],l,X,2,cl)==1 :
+            Y['Classe'][k]='Solution neutre'
+            c2+=1
+        else :
+            Y['Classe'][k]='Solution satisfaisante'
+            c3+=1
+    #print(c1/V1.shape[0],c2/V1.shape[0],c3/V1.shape[0])
+
+    Y.to_csv('Classification_instances_coeff_appris.csv', index=False)
+    return
+
+identify_classes_appris(instances=V,model=m,X=X,cl=cl)
--- UTA/UTA_functions.py
+++ UTA/UTA_functions.py
@@ -118,3 +118,105 @@ def si_k(m,X,u):
                 l.append(i)
                 i=[]
     return np.array(l)
+
+def add_constraints_app(list_alternatives,m,variables,X,X1,partial_categories,EPSILON,L):
+    s=variables["s"]
+    for p in partial_categories :
+        o=quicksum([s[l][X1[p[0]][l]] + ((list_alternatives[p[0]][l]-X[l][X1[p[0]][l]])/(X[l][X1[p[0]][l]+1]-X[l][X1[p[0]][l]]))*(s[l][X1[p[0]][l]+1]-s[l][X1[p[0]][l]]) for l in range(list_alternatives.shape[1])])
+        if p[1]==0:
+            m.addConstr(o-variables["eps_plus"][p[0]]+ variables["eps_minus"][p[0]] <= variables["cl"][0], name= "c_"+str(p[0])+str(0))
+        if p[1]==1:
+            m.addConstr(variables["cl"][0] + EPSILON <= o-variables["eps_plus"][p[0]]+ variables["eps_minus"][p[0]],name= "c_"+str(p[0])+str(1))
+            m.addConstr(o-variables["eps_plus"][p[0]]+ variables["eps_minus"][p[0]] <= variables["cl"][1],name= "c_"+str(p[0])+str(3))
+        if p[1]==2:
+            m.addConstr(variables["cl"][1] + EPSILON <= o-variables["eps_plus"][p[0]]+ variables["eps_minus"][p[0]],name= "c_"+str(p[0])+str(2))
+    m.addConstr(EPSILON<=variables["cl"][0],name= "cl_1")
+    m.addConstr(variables["cl"][0]+EPSILON <=variables["cl"][1],name="cl_2")
+    for i in range(list_alternatives.shape[1]):
+        m.addConstr(s[i][0]==0, name="intervalle"+str(i))
+        
+    for i in range(list_alternatives.shape[1]):
+        for k in range(L) :
+            m.addConstr(s[i][k+1]-s[i][k]>=EPSILON, name="coeffs" + str(i)+str(k)) #esp peut être différent de EPSILON mais on commence par le fixer égale à EPSILON
+    
+    m.addConstr(quicksum(s[i][L] for i in range(list_alternatives.shape[1]))==1,name="normalisation")
+    
+    return m
+
+def create_variables_app(m,list_alternatives,L):
+    #Attention Potentiellement ajouter le modèle en entrée contrairement à l'autre partie.
+    # Epsilon variables to deal with absolute values
+    eps_plus = np.array([m.addVar(name=
+                            "eps_plus_"
+                            + str(j),
+                            lb=0,
+                        )
+                        for j in range(list_alternatives.shape[0])
+        ],
+        dtype=object,
+    )
+    eps_minus = np.array([
+                            m.addVar(name=
+                            "eps_minus_"
+                            + str(j),
+                            lb=0,)
+                            for j in range(list_alternatives.shape[0])
+                            ],
+                        dtype=object,
+    )
+   
+    s = np.array(
+                [
+                    [
+                       m.addVar(name=
+                            "s_"
+                            + str(i)
+                            + ","
+                            + str(k),
+                            lb=0,
+                        )
+                for k in range(L+1)
+            ]
+            for i in range(list_alternatives.shape[1])
+        ],
+        dtype=object,
+    )
+    cl= np.array(
+                [
+                       m.addVar(name=
+                            "cl_"
+                            + str(k),
+                            lb=0,
+                        )
+                for k in range(2)
+            ],
+        dtype=object,
+    )
+    #eps= m.addVar(name="eps",lb=0,) # A voir si cela correspond à une contrainte ou si on l'impose
+    variables = {"s": s, "eps_plus": eps_plus, "eps_minus": eps_minus,"cl":cl}
+    return m,variables
+
+def get_cl(m,X):
+    cl=[]
+    for v in m.getVars():
+        if v.VarName[0]=="c":
+            cl.append(v.X)
+    cl=np.array(cl)
+    return cl
+
+def s_score_app(p,s,X,L,cl):
+    x1=[]
+    for j in range(len(p)): # Pour X1, X1 correspond à la valeur de k pour le coeff ij, j cirtère, i instance
+        for k in range(L):
+            if X[j][k]<=p[j]<= X[j][k+1] :
+                x1.append(int(k))
+    sm=0
+    for l in range(len(p)):
+        sm+=s[l][x1[l]] + ((p[l]-X[l][x1[l]])/(X[l][x1[l]+1]-X[l][x1[l]]))*(s[l][x1[l]+1]-s[l][x1[l]])
+    #return sm
+    if sm<=cl[0] :
+        return 0
+    elif cl[0]<sm<=cl[1] :
+        return 1
+    else :
+        return 2
