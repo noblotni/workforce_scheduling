2302730cefff2dbe48ad268ebefad19d1cdf7002
noblotni <noblotni@gmail.com>
2023-01-30 23:54:33 +0100

    Add UTA model.

--- workforce_scheduling/preferences/uta.py
+++ workforce_scheduling/preferences/uta.py
@@ -1,49 +1,34 @@
-# Basic modules
+"""Implement the UTA preference model."""
 import numpy as np
-import matplotlib.pyplot as plt
-
-# Module related to Gurobi
-from gurobipy import *
-import json
+import gurobipy as gb
 from pathlib import Path
 import pandas as pd
-from uta_functions import Built_model_instances
-from uta_functions import create_variables
-from uta_functions import add_constraints
-from uta_functions import add_objective_function
-from uta_functions import s_score
-from uta_functions import si_k
-from uta_functions import create_variables_app
-from uta_functions import add_constraints_app
-from uta_functions import s_score_app
-from uta_functions import get_cl
+from workforce_scheduling.preferences import uta_utils
 
-# Data directory
-DATA_PATH = Path("./Preorder_instance.csv")
-DATA_PATH1 = Path("./medium_instance_pareto.csv")
-
-I = pd.read_csv(DATA_PATH)
-I1 = I["Classe"].to_numpy()
-I0 = I.drop(["Classe"], axis=1).to_numpy()
-I1 = [[k, I1[k]] for k in range(len(I1))]
 
 EPSILON = 0.001
-
-############################################################################################
-######################## MODEL WITH FIXED COEFFICIENTS #####################################
-############################################################################################
+L = 2
 
 
-def create_model(list_alternatives, partial_categories, EPSILON, L):
-    m = Model("Preferences")
+def create_model(list_alternatives, partial_categories):
+    """Create model with fixed coefficients.
+    Args:
+        - list_alternatives: list of alternatives
+        -  partial_categories: predefined order
+    """
+    model = gb.Model("UTA")
 
-    instances = Built_model_instances(list_alternatives=list_alternatives, L=L)
+    instances = uta_utils.build_model_instances(
+        list_alternatives=list_alternatives, L=L
+    )
     X, X1 = instances
-    m, variables = create_variables(m=m, list_alternatives=list_alternatives, L=L)
+    model, variables = uta_utils.create_variables(
+        model=model, list_alternatives=list_alternatives, L=L
+    )
 
-    m = add_constraints(
+    model = uta_utils.add_constraints(
         list_alternatives=list_alternatives,
-        m=m,
+        model=model,
         variables=variables,
         X=X,
         X1=X1,
@@ -52,122 +37,150 @@ def create_model(list_alternatives, partial_categories, EPSILON, L):
         L=L,
     )
 
-    m = add_objective_function(m=m, variable=variables)
+    model = uta_utils.add_objective_function(model=model, variable=variables)
 
-    return m
+    return model
 
 
-m = create_model(I0, I1, EPSILON, 2)
-m.optimize()
+def identify_classes(instances, model, X):
+    """Assign classes to instances."""
+    V1 = instances.to_numpy()
+    V1 = V1[1:]
+    V1 = (V1.T[1:]).T
 
-# Creation of the list, in fact we recreate the sik matrix with the optimal values
+    l = uta_utils.si_k(model, X, u=3)
+    g = np.zeros(len(V1))
+    Y = np.c_[V1, g]
+    Y = pd.DataFrame(
+        Y, columns=["profit", "projects_done", "long_proj_duration", "class"]
+    )
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    for k in range(V1.shape[0]):
+        if uta_utils.s_score(V1[k], l, X, 2) == 0:
+            Y["class"][k] = "Non-acceptable"
+            c1 += 1
+        elif uta_utils.s_score(V1[k], l, X, 2) == 1:
+            Y["class"][k] = "Neutral"
+            c2 += 1
+        else:
+            Y["class"][k] = "Acceptable"
+            c3 += 1
+    Y.to_csv("Classification_instances.csv", index=False)
 
-X, X1 = Built_model_instances(list_alternatives=I0, L=2)
-l = si_k(m, X, u=3)
-# Be careful not to take coefficients outside the interval defined by min(i) and max(i)
 
-V = pd.read_csv(DATA_PATH1)
+def create_model_app(list_alternatives, partial_categories):
+    model = gb.Model("UTA")
+
+    instances = uta_utils.build_model_instances(
+        list_alternatives=list_alternatives, L=L
+    )
+    X, X1 = instances
+    model, variables = uta_utils.create_variables_app(
+        model=model, list_alternatives=list_alternatives, L=L
+    )
+
+    model = uta_utils.add_constraints_app(
+        list_alternatives=list_alternatives,
+        model=model,
+        variables=variables,
+        X=X,
+        X1=X1,
+        partial_categories=partial_categories,
+        EPSILON=EPSILON,
+        L=L,
+    )
+
+    model = uta_utils.add_objective_function(model=model, variable=variables)
+
+    return model
 
 
 def identify_classes(
     instances, model, X
 ):  # We create a function that will assign classes to our instances.
-
+    """Assign a class to solutions"""
     V1 = instances.to_numpy()
     V1 = V1[1:]
     V1 = (V1.T[1:]).T
 
-    l = si_k(model, X, u=3)
+    l = uta_utils.si_k(model, u=3)
     g = np.zeros(len(V1))
     Y = np.c_[V1, g]
     Y = pd.DataFrame(
-        Y, columns=["profit", "projects_done", "long_proj_duration", "Classe"]
+        Y, columns=["profit", "projects_done", "long_proj_duration", "class"]
     )
     c1 = 0
     c2 = 0
     c3 = 0
     for k in range(V1.shape[0]):
-        if s_score(V1[k], l, X, 2) == 0:
-            Y["Classe"][k] = "Solution non acceptable"
+        if uta_utils.s_score(V1[k], l, X, 2) == 0:
+            Y["class"][k] = "Non-acceptable"
             c1 += 1
-        elif s_score(V1[k], l, X, 2) == 1:
-            Y["Classe"][k] = "Solution neutre"
+        elif uta_utils.s_score(V1[k], l, X, 2) == 1:
+            Y["class"][k] = "Neutral"
             c2 += 1
         else:
-            Y["Classe"][k] = "Solution satisfaisante"
+            Y["class"][k] = "Acceptable"
             c3 += 1
-    # print(c1/V1.shape[0],c2/V1.shape[0],c3/V1.shape[0])
 
-    Y.to_csv("Classification_instances.csv", index=False)
-    return
+    Y.to_csv(Path("./Classification_instances.csv"), index=False)
 
 
-identify_classes(instances=V, model=m, X=X)
-
-############################################################################################
-######################## MODELE WITH LEARNT COEFFICIENTS ###################################
-############################################################################################
-
-
-def create_model_app(list_alternatives, partial_categories, EPSILON, L):
-    m = Model("Preferences")
-
-    instances = Built_model_instances(list_alternatives=list_alternatives, L=L)
-    X, X1 = instances
-    m, variables = create_variables_app(m=m, list_alternatives=list_alternatives, L=L)
-
-    m = add_constraints_app(
-        list_alternatives=list_alternatives,
-        m=m,
-        variables=variables,
-        X=X,
-        X1=X1,
-        partial_categories=partial_categories,
-        EPSILON=EPSILON,
-        L=L,
-    )
-
-    m = add_objective_function(m=m, variable=variables)
-
-    return m
-
-
-m_app = create_model_app(I0, I1, EPSILON, 2)
-m_app.optimize()
-
-l = si_k(m_app, X, u=3)
-cl = get_cl(m_app, X)
-
-
-def identify_classes_appris(instances, model, X, cl):
-
+def identify_classes_learnt(instances, model, X, coeffs_learnt):
+    """Assign classes with the model with learnt coefficients."""
     V1 = instances.to_numpy()
     V1 = V1[1:]
     V1 = (V1.T[1:]).T
 
-    l = si_k(model, X, u=3)
+    l = uta_utils.si_k(model, u=3)
     g = np.zeros(len(V1))
     Y = np.c_[V1, g]
     Y = pd.DataFrame(
-        Y, columns=["profit", "projects_done", "long_proj_duration", "Classe"]
+        Y, columns=["profit", "projects_done", "long_proj_duration", "class"]
     )
-    # print(cl)
     c1 = 0
     c2 = 0
     c3 = 0
     for k in range(V1.shape[0]):
-        if s_score_app(V1[k], l, X, 2, cl) == 0:
-            Y["Classe"][k] = "Solution non acceptable"
+        if uta_utils.s_score_learnt(V1[k], l, X, 2, coeffs_learnt) == 0:
+            Y["class"][k] = "Non acceptable"
             c1 += 1
-        elif s_score_app(V1[k], l, X, 2, cl) == 1:
-            Y["Classe"][k] = "Solution neutre"
+        elif uta_utils.s_score_learnt(V1[k], l, X, 2, coeffs_learnt) == 1:
+            Y["class"][k] = "Neutral"
             c2 += 1
         else:
-            Y["Classe"][k] = "Solution satisfaisante"
+            Y["class"][k] = "Acceptable"
             c3 += 1
-    # print(c1/V1.shape[0],c2/V1.shape[0],c3/V1.shape[0])
-    Y.to_csv("Classification_instances_coeff_appris.csv", index=False)
-    return
+    Y.to_csv(Path("./Classification_instances_coeff_appris.csv"), index=False)
 
 
-identify_classes_appris(instances=V, model=m, X=X, cl=cl)
+def run_uta(pareto_path: Path, preorder_path: Path):
+    """Run UTA preferences model.
+
+    Args:
+        - pareto_path (Path): path to the Pareto surface.
+        - preorder_path (Path): path to a preorder of a subset of solutions
+    """
+    preorder_df = pd.read_csv(preorder_path)
+    preorder_cat = preorder_df["class"]
+    list_alternatives = preorder_df.drop(["class"], axis=1).to_numpy()
+    preorder_cat = [[k, preorder_cat[k]] for k in range(len(preorder_cat))]
+    # Model with constant coefficients
+    model = create_model(list_alternatives, preorder_cat)
+    model.optimize()
+    # Creation of the list, in fact we recreate the sik matrix with the optimal values
+    X, _ = uta_utils.build_model_instances(list_alternatives=list_alternatives, L=2)
+    # Be careful not to take coefficients outside the interval defined by min(i) and max(i)
+    pareto_df = pd.read_csv(pareto_path)
+    identify_classes(instances=pareto_df, model=model, X=X)
+
+    # Model with learnt coefficients
+    model_coeffs_learnt = create_model_app(list_alternatives, preorder_cat)
+    model_coeffs_learnt.optimize()
+
+    coeffs_learnt = uta_utils.get_coeffs_learnt(model_coeffs_learnt)
+    identify_classes_learnt(
+        instances=pareto_df, model=model_coeffs_learnt, X=X, coeffs_learnt=coeffs_learnt
+    )
---
+++ workforce_scheduling/preferences/uta_utils.py
@@ -0,0 +1,304 @@
+"""Contain useful functions to implement UTA preference model."""
+import numpy as np
+import gurobipy as gb
+
+
+def create_variables(model, list_alternatives, L):
+    """Create variables for the model."""
+    # Attention Potentially add the model as input unlike the other part.
+    # Epsilon variables to deal with absolute values
+    eps_plus = np.array(
+        [
+            model.addVar(
+                name="eps_plus_" + str(j),
+                lb=0,
+            )
+            for j in range(list_alternatives.shape[0])
+        ],
+        dtype=object,
+    )
+    eps_minus = np.array(
+        [
+            model.addVar(
+                name="eps_minus_" + str(j),
+                lb=0,
+            )
+            for j in range(list_alternatives.shape[0])
+        ],
+        dtype=object,
+    )
+    s = np.array(
+        [
+            [
+                model.addVar(
+                    name="s_" + str(i) + "," + str(k),
+                    lb=0,
+                )
+                for k in range(L + 1)
+            ]
+            for i in range(list_alternatives.shape[1])
+        ],
+        dtype=object,
+    )
+    variables = {"s": s, "eps_plus": eps_plus, "eps_minus": eps_minus}
+    return model, variables
+
+
+def add_constraints(
+    list_alternatives, model, variables, X, X1, partial_categories, EPSILON, L
+):  # For s(j) potentially generated a new matrix with the coefficient (xij-xik)/(xik+1-xik) which is actually determined at the beginning of the algorithm
+    s = variables["s"]
+    for p in partial_categories:
+        o = gb.quicksum(
+            [
+                s[l][X1[p[0]][l]]
+                + (
+                    (list_alternatives[p[0]][l] - X[l][X1[p[0]][l]])
+                    / (X[l][X1[p[0]][l] + 1] - X[l][X1[p[0]][l]])
+                )
+                * (s[l][X1[p[0]][l] + 1] - s[l][X1[p[0]][l]])
+                for l in range(list_alternatives.shape[1])
+            ]
+        )
+        if p[1] == 0:
+            model.addConstr(
+                o - variables["eps_plus"][p[0]] + variables["eps_minus"][p[0]] <= 0.33,
+                name="c_" + str(p[0]) + str(0),
+            )
+        if p[1] == 1:
+            model.addConstr(
+                0.33 + EPSILON
+                <= o - variables["eps_plus"][p[0]] + variables["eps_minus"][p[0]],
+                name="c_" + str(p[0]) + str(1),
+            )
+            model.addConstr(
+                o - variables["eps_plus"][p[0]] + variables["eps_minus"][p[0]] <= 0.66,
+                name="c_" + str(p[0]) + str(3),
+            )
+        if p[1] == 2:
+            model.addConstr(
+                0.66 + EPSILON
+                <= o - variables["eps_plus"][p[0]] + variables["eps_minus"][p[0]],
+                name="c_" + str(p[0]) + str(2),
+            )
+    for i in range(list_alternatives.shape[1]):
+        model.addConstr(s[i][0] == 0, name="intervalle" + str(i))
+
+    for i in range(list_alternatives.shape[1]):
+        for k in range(L):
+            model.addConstr(
+                s[i][k + 1] - s[i][k] >= EPSILON, name="coeffs" + str(i) + str(k)
+            )  # esp can be different from EPSILON but we start by setting it equal to EPSILON
+
+    model.addConstr(
+        gb.quicksum(s[i][L] for i in range(list_alternatives.shape[1])) == 1,
+        name="normalisation",
+    )
+
+    return model
+
+
+def build_model_instances(
+    list_alternatives, L
+):  # Cf calculation of s(j) / For this we define xik (the bounds of the intervals) and xijk (the k corresponding to xij: 3 dimensions.)
+
+    X = np.zeros((list_alternatives.shape[1], L + 1))
+    O = list_alternatives.T
+    for i in range(
+        list_alternatives.shape[1]
+    ):  # i this column of list_alternatives becomes a row for X
+        for k in range(L + 1):
+            X[i][k] = min(O[i]) + (k / L) * (max(O[i]) - min(O[i]))
+    X1 = np.ones(list_alternatives.shape, dtype=int)
+    for i in range(list_alternatives.shape[0]):
+        for j in range(
+            list_alternatives.shape[1]
+        ):  # For X1, X1 corresponds to the value of k for the coefficient ij, j cirtère, i instance
+            for k in range(L):
+                if X[j][k] <= list_alternatives[i][j] <= X[j][k + 1]:
+                    X1[i][j] = k
+    return X, X1
+
+
+def add_objective_function(model, variable):
+    suma = gb.quicksum(
+        [
+            variable["eps_plus"][i] + variable["eps_minus"][i]
+            for i in range(len(variable["eps_plus"]))
+        ]
+    )
+    model.setObjective(suma, gb.GRB.MINIMIZE)
+    return model
+
+
+def s_score(p, s, X, L):
+    x1 = []
+    for j in range(
+        len(p)
+    ):  # For X1, X1 corresponds to the value of k for the coefficient ij, j cirtère, i instance
+        for k in range(L):
+            if X[j][k] <= p[j] <= X[j][k + 1]:
+                x1.append(int(k))
+    sm = 0
+    for l in range(len(p)):
+        sm += s[l][x1[l]] + ((p[l] - X[l][x1[l]]) / (X[l][x1[l] + 1] - X[l][x1[l]])) * (
+            s[l][x1[l] + 1] - s[l][x1[l]]
+        )
+    if sm <= 0.33:
+        return 0
+    elif 0.33 < sm <= 0.66:
+        return 1
+    else:
+        return 2
+
+
+def si_k(model, u):
+    l = []
+    c = 0
+    i = []
+    for v in model.getVars():
+        if v.VarName[0] == "s":
+            c += 1
+            i.append(v.X)
+            if c % u == 0:
+                l.append(i)
+                i = []
+    return np.array(l)
+
+
+def add_constraints_app(
+    list_alternatives, model, variables, X, X1, partial_categories, EPSILON, L
+):
+    s = variables["s"]
+    for p in partial_categories:
+        o = gb.quicksum(
+            [
+                s[l][X1[p[0]][l]]
+                + (
+                    (list_alternatives[p[0]][l] - X[l][X1[p[0]][l]])
+                    / (X[l][X1[p[0]][l] + 1] - X[l][X1[p[0]][l]])
+                )
+                * (s[l][X1[p[0]][l] + 1] - s[l][X1[p[0]][l]])
+                for l in range(list_alternatives.shape[1])
+            ]
+        )
+        if p[1] == 0:
+            model.addConstr(
+                o - variables["eps_plus"][p[0]] + variables["eps_minus"][p[0]]
+                <= variables["cl"][0],
+                name="c_" + str(p[0]) + str(0),
+            )  # Introduction of coefficients "cl" as variables because we want to learn them.
+        if p[1] == 1:
+            model.addConstr(
+                variables["cl"][0] + EPSILON
+                <= o - variables["eps_plus"][p[0]] + variables["eps_minus"][p[0]],
+                name="c_" + str(p[0]) + str(1),
+            )
+            model.addConstr(
+                o - variables["eps_plus"][p[0]] + variables["eps_minus"][p[0]]
+                <= variables["cl"][1],
+                name="c_" + str(p[0]) + str(3),
+            )
+        if p[1] == 2:
+            model.addConstr(
+                variables["cl"][1] + EPSILON
+                <= o - variables["eps_plus"][p[0]] + variables["eps_minus"][p[0]],
+                name="c_" + str(p[0]) + str(2),
+            )
+    model.addConstr(0.15 <= variables["cl"][0], name="cl_1")
+    model.addConstr(variables["cl"][0] + EPSILON <= variables["cl"][1], name="cl_2")
+    for i in range(list_alternatives.shape[1]):
+        model.addConstr(s[i][0] == 0, name="intervalle" + str(i))
+
+    for i in range(list_alternatives.shape[1]):
+        for k in range(L):
+            model.addConstr(
+                s[i][k + 1] - s[i][k] >= EPSILON, name="coeffs" + str(i) + str(k)
+            )
+
+    model.addConstr(
+        gb.quicksum(s[i][L] for i in range(list_alternatives.shape[1])) == 1,
+        name="normalisation",
+    )
+
+    return model
+
+
+def create_variables_app(model, list_alternatives, L):
+    """Create variables for the model with learnt coefficients."""
+    eps_plus = np.array(
+        [
+            model.addVar(
+                name="eps_plus_" + str(j),
+                lb=0,
+            )
+            for j in range(list_alternatives.shape[0])
+        ],
+        dtype=object,
+    )
+    eps_minus = np.array(
+        [
+            model.addVar(
+                name="eps_minus_" + str(j),
+                lb=0,
+            )
+            for j in range(list_alternatives.shape[0])
+        ],
+        dtype=object,
+    )
+
+    s = np.array(
+        [
+            [
+                model.addVar(
+                    name="s_" + str(i) + "," + str(k),
+                    lb=0,
+                )
+                for k in range(L + 1)
+            ]
+            for i in range(list_alternatives.shape[1])
+        ],
+        dtype=object,
+    )
+    cl = np.array(
+        [
+            model.addVar(
+                name="cl_" + str(k),
+                lb=0,
+            )
+            for k in range(2)
+        ],
+        dtype=object,
+    )
+    variables = {"s": s, "eps_plus": eps_plus, "eps_minus": eps_minus, "cl": cl}
+    return model, variables
+
+
+def get_coeffs_learnt(
+    model,
+):
+    """Get the learnt coefficients after solving the problem with Gurobi."""
+    coeffs_learnt = []
+    for v in model.getVars():
+        if v.VarName[0] == "c":
+            coeffs_learnt.append(v.X)
+    return np.array(coeffs_learnt)
+
+
+def s_score_learnt(p, s, X, L, cl):
+    x1 = []
+    for j in range(len(p)):
+        for k in range(L):
+            if X[j][k] <= p[j] <= X[j][k + 1]:
+                x1.append(int(k))
+    sm = 0
+    for l in range(len(p)):
+        sm += s[l][x1[l]] + ((p[l] - X[l][x1[l]]) / (X[l][x1[l] + 1] - X[l][x1[l]])) * (
+            s[l][x1[l] + 1] - s[l][x1[l]]
+        )
+    if sm <= cl[0]:
+        return 0
+    elif cl[0] < sm <= cl[1]:
+        return 1
+    else:
+        return 2
